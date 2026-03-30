762027dfd244a62548231f6da6ada3315002c1d2
Ilia Beliaev <i.beliaev@activevideo.com>
2026-03-30 22:44:35 +0200

    Updates

--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module "*.geojson" {
-  import type { FeatureCollection } from "geojson";
-  const value: FeatureCollection;
-  export default value;
-}
